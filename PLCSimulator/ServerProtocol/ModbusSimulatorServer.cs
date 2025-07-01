21fd5c42426c41ccec33a90d88d37840a9a35354
hwlee622 <leblight@gmail.com>
2025-07-01 13:16:30 +0900

    Modbus 영역 주소값 매핑

--- PLCSimulator/ServerProtocol/ModbusSimulatorServer.cs
+++ PLCSimulator/ServerProtocol/ModbusSimulatorServer.cs
@@ -42,35 +42,35 @@ namespace PLCSimulator
                 {
                     Thread.Sleep(10);
 
-                    for (int i = 0; i < prevCoils.Length; i++)
+                    for (int i = 1; i < prevCoils.Length; i++)
                     {
                         var coil = m_server.coils[i];
                         if (prevCoils[i] != coil)
-                            DataManager.Instance.BitDataDict[COIL].SetData(i, new bool[] { coil });
+                            DataManager.Instance.BitDataDict[COIL].SetData(i - 1, new bool[] { coil });
                         prevCoils[i] = coil;
                     }
 
-                    for (int i = 0; i < prevHRegister.Length; i++)
+                    for (int i = 1; i < prevHRegister.Length; i++)
                     {
                         var register = m_server.holdingRegisters[i];
                         if (prevHRegister[i] != register)
-                            DataManager.Instance.WordDataDict[HOLDING_REGISTER].SetData(i, new ushort[] { (ushort)register });
+                            DataManager.Instance.WordDataDict[HOLDING_REGISTER].SetData(i - 1, new ushort[] { (ushort)register });
                         prevHRegister[i] = register;
                     }
 
-                    for (int i = 0; i < prevBits.Length; i++)
+                    for (int i = 0; i < prevBits.Length - 1; i++)
                     {
                         var bit = DataManager.Instance.BitDataDict[COIL].GetData(i, 1)[0];
                         if (prevBits[i] != bit)
-                            m_server.coils[i] = bit;
+                            m_server.coils[i + 1] = bit;
                         prevBits[i] = bit;
                     }
 
-                    for (int i = 0; i < prevWords.Length; i++)
+                    for (int i = 0; i < prevWords.Length - 1; i++)
                     {
                         var word = DataManager.Instance.WordDataDict[HOLDING_REGISTER].GetData(i, 1)[0];
                         if (prevWords[i] != word)
-                            m_server.holdingRegisters[i] = (short)word;
+                            m_server.holdingRegisters[i + 1] = (short)word;
                         prevWords[i] = word;
                     }
                 }
