e6a5dd7f0a17bc94734481a54e7e4ac60bb21ef6
hwlee622 <leblight@gmail.com>
2025-07-02 17:11:39 +0900

    Modbus 서버 동기화 로직 보완 (#6)
    동기화 될 데이터와 이미 동기화 되어 있다면 동기화 실행하지 않음

--- PLCSimulator/ServerProtocol/ModbusSimulatorServer.cs
+++ PLCSimulator/ServerProtocol/ModbusSimulatorServer.cs
@@ -33,6 +33,9 @@ namespace PLCSimulator
 
         private void SyncData()
         {
+            var coilData = DataManager.Instance.BitDataDict[COIL];
+            var holdingRegisterData = DataManager.Instance.WordDataDict[HOLDING_REGISTER];
+
             bool[] prevCoils = new bool[DataManager.Instance.BitDataDict[COIL].DataLength];
             short[] prevHRegister = new short[DataManager.Instance.WordDataDict[HOLDING_REGISTER].DataLength];
             bool[] prevBits = new bool[DataManager.Instance.BitDataDict[COIL].DataLength];
@@ -46,31 +49,31 @@ namespace PLCSimulator
                     for (int i = 1; i < prevCoils.Length; i++)
                     {
                         var coil = m_server.coils[i];
-                        if (prevCoils[i] != coil)
-                            DataManager.Instance.BitDataDict[COIL].SetData(i - 1, new bool[] { coil });
+                        if (prevCoils[i] != coil && coilData.GetData(i - 1, 1)[0] != coil)
+                            coilData.SetData(i - 1, new bool[] { coil });
                         prevCoils[i] = coil;
                     }
 
                     for (int i = 1; i < prevHRegister.Length; i++)
                     {
                         var register = m_server.holdingRegisters[i];
-                        if (prevHRegister[i] != register)
-                            DataManager.Instance.WordDataDict[HOLDING_REGISTER].SetData(i - 1, new ushort[] { (ushort)register });
+                        if (prevHRegister[i] != register && holdingRegisterData.GetData(i - 1, 1)[0] != (ushort)register)
+                            holdingRegisterData.SetData(i - 1, new ushort[] { (ushort)register });
                         prevHRegister[i] = register;
                     }
 
                     for (int i = 0; i < prevBits.Length - 1; i++)
                     {
-                        var bit = DataManager.Instance.BitDataDict[COIL].GetData(i, 1)[0];
-                        if (prevBits[i] != bit)
+                        var bit = coilData.GetData(i, 1)[0];
+                        if (prevBits[i] != bit && m_server.coils[i + 1] != bit)
                             m_server.coils[i + 1] = bit;
                         prevBits[i] = bit;
                     }
 
                     for (int i = 0; i < prevWords.Length - 1; i++)
                     {
-                        var word = DataManager.Instance.WordDataDict[HOLDING_REGISTER].GetData(i, 1)[0];
-                        if (prevWords[i] != word)
+                        var word = holdingRegisterData.GetData(i, 1)[0];
+                        if (prevWords[i] != word && m_server.holdingRegisters[i + 1] != word)
                             m_server.holdingRegisters[i + 1] = (short)word;
                         prevWords[i] = word;
                     }
