3b8399ef155aa548faaa6b13201c120d0db70fe7
hwlee622 <leblight@gmail.com>
2025-07-17 18:01:10 +0900

    Modbus DiscreteInput, InputRegister 추가 (#7)
    Modbus 시뮬레이션 시 DiscreteInput과 InputRegister 추가로 지원

--- PLCSimulator/ServerProtocol/ModbusSimulatorServer.cs
+++ PLCSimulator/ServerProtocol/ModbusSimulatorServer.cs
@@ -1,13 +1,15 @@
 ﻿using EasyModbus;
 using System;
 using System.Net;
-using System.Threading;
+using System.Threading.Tasks;
 
 namespace PLCSimulator
 {
     public class ModbusSimulatorServer
     {
         public const string COIL = "COIL";
+        private const string DISCRETE_INPUT = "INPUT";
+        private const string INPUT_REGISTER = "IR";
         public const string HOLDING_REGISTER = "HR";
 
         private ModbusServer m_server;
@@ -23,59 +25,88 @@ namespace PLCSimulator
             m_server.Listen();
 
             DataManager.Instance.BitDataDict.Add(COIL, new DataManager.BitData(10000));
+            DataManager.Instance.BitDataDict.Add(DISCRETE_INPUT, new DataManager.BitData(10000));
+            DataManager.Instance.WordDataDict.Add(INPUT_REGISTER, new DataManager.WordData(10000));
             DataManager.Instance.WordDataDict.Add(HOLDING_REGISTER, new DataManager.WordData(10000));
 
-            var thread = new Thread(SyncData);
-            thread.IsBackground = true;
-            thread.Start();
-
+            Task.Run(() => SyncData());
         }
 
-        private void SyncData()
+        private async Task SyncData()
         {
             var coilData = DataManager.Instance.BitDataDict[COIL];
+            var inputData = DataManager.Instance.BitDataDict[DISCRETE_INPUT];
+            var inputRegisterData = DataManager.Instance.WordDataDict[INPUT_REGISTER];
             var holdingRegisterData = DataManager.Instance.WordDataDict[HOLDING_REGISTER];
 
-            bool[] prevCoils = new bool[DataManager.Instance.BitDataDict[COIL].DataLength];
-            short[] prevHRegister = new short[DataManager.Instance.WordDataDict[HOLDING_REGISTER].DataLength];
-            bool[] prevBits = new bool[DataManager.Instance.BitDataDict[COIL].DataLength];
-            ushort[] prevWords = new ushort[DataManager.Instance.WordDataDict[HOLDING_REGISTER].DataLength];
+            bool[] preCoils = new bool[coilData.DataLength];
+            bool[] preInputs = new bool[inputData.DataLength];
+            ushort[] preInputRegisters = new ushort[inputRegisterData.DataLength];
+            ushort[] preHoldingRegisters = new ushort[holdingRegisterData.DataLength];
             while (true)
             {
                 try
                 {
-                    Thread.Sleep(10);
+                    await Task.Delay(10);
 
-                    for (int i = 1; i < prevCoils.Length; i++)
+                    for (int i = 0; i < preCoils.Length - 1; i++)
                     {
-                        var coil = m_server.coils[i];
-                        if (prevCoils[i] != coil && coilData.GetData(i - 1, 1)[0] != coil)
-                            coilData.SetData(i - 1, new bool[] { coil });
-                        prevCoils[i] = coil;
-                    }
-
-                    for (int i = 1; i < prevHRegister.Length; i++)
-                    {
-                        var register = m_server.holdingRegisters[i];
-                        if (prevHRegister[i] != register && holdingRegisterData.GetData(i - 1, 1)[0] != (ushort)register)
-                            holdingRegisterData.SetData(i - 1, new ushort[] { (ushort)register });
-                        prevHRegister[i] = register;
-                    }
-
-                    for (int i = 0; i < prevBits.Length - 1; i++)
-                    {
+                        var coil = m_server.coils[i + 1];
                         var bit = coilData.GetData(i, 1)[0];
-                        if (prevBits[i] != bit && m_server.coils[i + 1] != bit)
-                            m_server.coils[i + 1] = bit;
-                        prevBits[i] = bit;
+                        bool value;
+                        if (coil == bit)
+                            value = coil;
+                        else
+                            value = coil == preCoils[i] ? bit : coil;
+
+                        coilData.SetData(i, new bool[] { value });
+                        m_server.coils[i + 1] = value;
+                        preCoils[i] = value;
                     }
 
-                    for (int i = 0; i < prevWords.Length - 1; i++)
+                    for (int i = 0; i < preInputs.Length - 1; i++)
                     {
+                        var input = m_server.discreteInputs[i + 1];
+                        var bit = inputData.GetData(i, 1)[0];
+                        bool value;
+                        if (input == bit)
+                            value = input;
+                        else
+                            value = input == preInputs[i] ? bit : input;
+
+                        inputData.SetData(i, new bool[] { value });
+                        m_server.discreteInputs[i + 1] = value;
+                        preInputs[i] = value;
+                    }
+
+                    for (int i = 0; i < preInputRegisters.Length - 1; i++)
+                    {
+                        var register = (ushort)m_server.inputRegisters[i + 1];
+                        var word = inputRegisterData.GetData(i, 1)[0];
+                        ushort value;
+                        if (register == word)
+                            value = register;
+                        else
+                            value = register == preInputRegisters[i] ? word : register;
+
+                        inputRegisterData.SetData(i, new ushort[] { value });
+                        m_server.inputRegisters[i + 1] = (short)value;
+                        preInputRegisters[i] = value;
+                    }
+
+                    for (int i = 0; i < preHoldingRegisters.Length - 1; i++)
+                    {
+                        var register = (ushort)m_server.holdingRegisters[i + 1];
                         var word = holdingRegisterData.GetData(i, 1)[0];
-                        if (prevWords[i] != word && m_server.holdingRegisters[i + 1] != word)
-                            m_server.holdingRegisters[i + 1] = (short)word;
-                        prevWords[i] = word;
+                        ushort value;
+                        if (register == word)
+                            value = register;
+                        else
+                            value = register == preHoldingRegisters[i] ? word : register;
+
+                        holdingRegisterData.SetData(i, new ushort[] { value });
+                        m_server.holdingRegisters[i + 1] = (short)value;
+                        preHoldingRegisters[i] = value;
                     }
                 }
                 catch (Exception ex)
